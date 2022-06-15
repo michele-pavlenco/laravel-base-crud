d9cd8ff6558f95e8ceab2dc3bdd2d9bd4d56a04b
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-06-15 18:38:59 +0200

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,25 @@
 @extends('layouts.default')
 
 @section('pageTitle', 'Fumetti - Home')
-    
+    <main></main>
 @section('mainContent')
-<main>
-    <h1>Fumetti</h1>
+<h2>Lista fumetti</h2>
+@foreach ($comics as $comic)
+<div class="card">
+    <div class="img">
+        <a href="">
+        <img src="{{$comic->thumb}}" alt="{{$comic->title}}">
+    </a>
+        <h3>{{$comic->title}}</h3>
+    </div>
+    <div class="card-content">
+        <p>{{$comic->description}}</p>
+        <p>{{$comic->series}}</p>
+        <p class="price">{{$comic->price}}</p>
+        <p>{{$comic->sale_date}}</p>
+        <p>{{$comic->type}}</p>
+    </div>
+</div>
+@endforeach
 </main>
-
 @endsection
